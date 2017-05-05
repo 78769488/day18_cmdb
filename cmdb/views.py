d9eb5c1897d59727175f86a873a8c88e15cc14a2
78769488@qq.com <mybaby1112>
2017-05-05 18:31:34 +0800

    0505

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def login(request):
+    # request包含用户(浏览器)提交过来的所有信息
+    error_msg = ""
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        if username == "hongyu_951@163.com" and password == "123456":
+            return redirect('/index/')
+        else:
+            error_msg = "用户名或密码错误！"
+    return render(request,
+                  'login.html',
+                  {"error_msg": error_msg})
+
+
+def index(request):
+    return render(request,
+                  'index.html')
